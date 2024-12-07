a5790ed1fe96c1d5d5bd6ec0e06279d8acbc5fe0
fabiobocchini <iambiozzus@gmail.com>
2024-12-07 21:37:13 +0100

    d02

---
+++ src/d02p1.kt
@@ -0,0 +1,35 @@
+private fun isRowSafe(row: List<Int>): Boolean {
+    var prev = row.first()
+    var direction: Char? = null
+
+    for (i in 1..<row.size) {
+        val next = row[i]
+
+        if (direction == null) {
+            direction = when {
+                prev < next -> 'g'
+                else -> 'l'
+            }
+        }
+
+        if (
+            (direction == 'l' && prev > next && prev - next <= 3) ||
+            (direction == 'g' && prev < next && next - prev <= 3)
+        ) {
+            prev = next
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+private fun solver(rows: List<String>): Int {
+    return rows.map {
+        it.split(" ").filter(String::isNotEmpty).map(String::toInt)
+    }.count(::isRowSafe)
+}
+
+fun main() {
+    Problem(2, 2).solve(::solver)
+}
---
+++ src/d02p2.kt
@@ -0,0 +1,54 @@
+private fun isRowSafe(row: List<Int>): Boolean {
+    var prev = row.first()
+    var direction: Char? = null
+
+    for (i in 1..<row.size) {
+        val next = row[i]
+
+        if (direction == null) {
+            direction = when {
+                prev < next -> 'g'
+                else -> 'l'
+            }
+        }
+
+        if (
+            (direction == 'l' && prev > next && prev - next <= 3) ||
+            (direction == 'g' && prev < next && next - prev <= 3)
+        ) {
+            prev = next
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+private fun dampenAndSolve(row: List<Int>): Boolean {
+    for (i in row.indices) {
+        row.toMutableList().removeAt(i)
+        if (isRowSafe(row.filterIndexed { index, _ -> index != i })) {
+            return true
+        }
+    }
+    return false
+}
+
+private fun solver(rows: List<String>): Int {
+    val parsedRows = rows.map {
+        it.split(" ").filter(String::isNotEmpty).map(String::toInt)
+    }
+
+    var acc = 0
+
+    for (row in parsedRows) {
+        if (isRowSafe(row) || dampenAndSolve(row)) {
+            acc++
+        }
+    }
+    return acc
+}
+
+fun main() {
+    Problem(2, 4).solve(::solver)
+}
