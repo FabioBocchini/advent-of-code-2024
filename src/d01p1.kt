d2d635ea5c94ad00da1a082ab10c116f58e5c647
fabiobocchini <iambiozzus@gmail.com>
2024-12-07 15:07:55 +0100

    d01p1

---
+++ src/d01p1.kt
@@ -0,0 +1,29 @@
+fun solver0101(rows: List<String>): Int {
+    val fst = mutableListOf<Int>()
+    val snd = mutableListOf<Int>()
+
+    for (row in rows) {
+        val splitRow = row.split(" ").filter(String::isNotEmpty)
+        fst.add(splitRow[0].toInt())
+        snd.add(splitRow[1].toInt())
+    }
+
+    fst.sort()
+    snd.sort()
+
+    var acc = 0
+
+    for (i in rows.indices) {
+        acc += if (fst[i] <= snd[i]) {
+            snd[i] - fst[i]
+        } else {
+            fst[i] - snd[i]
+        }
+    }
+
+    return acc
+}
+
+fun main() {
+    Problem(1, 11).solve(::solver0101)
+}
---
+++ src/utils.kt
@@ -0,0 +1,28 @@
+import java.io.File
+import kotlin.time.TimeSource
+
+class Problem(day: Int, private val expectedTestResult: Int) {
+    private val fileName = when {
+        day < 10 -> "0$day"
+        else -> "$day"
+    }
+    private val testPath = "./tests/$fileName"
+    private val inputPath = "./inputs/$fileName"
+
+    fun solve(solver: (List<String>) -> Int) {
+        val testResult = solver(File(testPath).readLines())
+
+        if (testResult != expectedTestResult) {
+            println("test unsuccessful\nexpected: $expectedTestResult\nbut got: $testResult")
+            return
+        }
+
+        val timeSource = TimeSource.Monotonic
+        val start = timeSource.markNow()
+        val result = solver(File(inputPath).readLines())
+        val end = timeSource.markNow()
+
+        println("time elapsed: ${end - start}")
+        println("result: $result")
+    }
+}
