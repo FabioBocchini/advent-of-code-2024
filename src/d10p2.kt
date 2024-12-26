6d10d197b01194c207efbf026c7e8d6a18a67891
fabiobocchini <iambiozzus@gmail.com>
2024-12-26 11:39:05 +0100

    d10

---
+++ src/d10p2.kt
@@ -0,0 +1,64 @@
+package d10p2
+
+import utils.Problem
+
+private typealias Grid = MutableList<List<Int>>
+private typealias Pos = Pair<Int, Int>
+
+private val steps: Map<String, Pos> = mapOf(
+    "u" to Pos(-1, 0),
+    "l" to Pos(0, -1),
+    "r" to Pos(0, 1),
+    "d" to Pos(1, 0),
+)
+
+private fun parseGrid(input: List<String>): Pair<Grid, List<Pos>> {
+    val grid: Grid = mutableListOf()
+    val startingPos: MutableList<Pos> = mutableListOf()
+
+    for (y in input.indices) {
+        val row = mutableListOf<Int>()
+        for (x in input[0].indices) {
+            val value = input[y][x].digitToInt()
+            row.add(value)
+            if (value == 0) {
+                startingPos.add(Pos(y, x))
+            }
+        }
+        grid.add(row)
+    }
+    return Pair(grid, startingPos)
+}
+
+private fun countScore(grid: Grid, toVisit: ArrayDeque<Pos>, endsFound: Long = 0L): Long {
+    if (toVisit.isEmpty()) {
+        return endsFound
+    }
+    val (y, x) = toVisit.removeFirst()
+    val value = grid[y][x]
+    if (value == 9) {
+        return countScore(grid, toVisit, endsFound + 1)
+    }
+
+    for ((dy, dx) in steps.values) {
+        try {
+            if (grid[y + dy][x + dx] == value + 1) {
+                toVisit.addFirst(Pos(y + dy, x + dx))
+            }
+        } catch (e: Exception) {
+            continue
+        }
+    }
+
+    return countScore(grid, toVisit, endsFound)
+}
+
+
+private fun solver(input: List<String>): Long {
+    val (grid, startingPositions) = parseGrid(input)
+    return startingPositions.sumOf { countScore(grid, ArrayDeque(listOf(it))) }
+}
+
+fun main() {
+    Problem(10, 81L).solve(::solver)
+}
