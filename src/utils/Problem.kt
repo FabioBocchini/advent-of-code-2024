fb28157868e5b16e1aff750b7f6c0278df0585a3
fabiobocchini <iambiozzus@gmail.com>
2024-12-29 13:29:21 +0100

    feat: problem may solve without test

--- src/utils/Problem.kt
+++ src/utils/Problem.kt
@@ -3,7 +3,7 @@ package utils
 import java.io.File
 import kotlin.time.TimeSource
 
-class Problem<N>(day: Int, private val expectedTestResult: N) {
+class Problem<N>(day: Int, private val expectedTestResult: N?) {
     private val fileName = when {
         day < 10 -> "0$day"
         else -> "$day"
@@ -12,13 +12,14 @@ class Problem<N>(day: Int, private val expectedTestResult: N) {
     private val inputPath = "./inputs/$fileName"
 
     private fun <T> solve(solver: (T) -> N, testInput: T, mainInput: T) {
-        val testResult = solver(testInput)
+        if (expectedTestResult != null) {
+            val testResult = solver(testInput)
 
-        if (testResult != expectedTestResult) {
-            println("test unsuccessful\nexpected: $expectedTestResult\nbut got: $testResult")
-            return
+            if (testResult != expectedTestResult) {
+                println("test unsuccessful\nexpected: $expectedTestResult\nbut got: $testResult")
+                return
+            }
         }
-
         val timeSource = TimeSource.Monotonic
         val start = timeSource.markNow()
         val result = solver(mainInput)
